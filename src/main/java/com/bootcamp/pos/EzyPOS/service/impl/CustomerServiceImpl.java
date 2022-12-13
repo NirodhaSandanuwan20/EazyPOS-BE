4fd74fa340f12041c2ac631855ab5b8acde40b63
ozen-hub <info.ozenhub@gmail.com>
2022-12-13 23:31:42 +0530

    7

--- src/main/java/com/bootcamp/pos/EzyPOS/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/bootcamp/pos/EzyPOS/service/impl/CustomerServiceImpl.java
@@ -4,6 +4,7 @@ import com.bootcamp.pos.EzyPOS.dto.request.CustomerDto;
 import com.bootcamp.pos.EzyPOS.entity.Customer;
 import com.bootcamp.pos.EzyPOS.repo.CustomerRepo;
 import com.bootcamp.pos.EzyPOS.service.CustomerService;
+import com.bootcamp.pos.EzyPOS.util.IdGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +14,22 @@ import org.springframework.stereotype.Service;
 public class CustomerServiceImpl implements CustomerService {
     @Autowired
     private CustomerRepo customerRepo;
+    @Autowired
+    private IdGenerator idGenerator;
 
     @Override
     public String saveCustomer(CustomerDto dto) {
-        // dto = > entity ==> save
-        // id generate
+        // kamal => KML==> ( save data -> customer)==> KML-C-1
+        // kamal => KML==> ( save data -> customer)==> KML-C-2
+
+        // Jayantha => JXY==> ( save data -> customer)==> JXY-C-1
+
         Customer c1 = new Customer(
-                "1d",dto.getName(), dto.getAddress(), dto.getSalary()
+                idGenerator.generateId(10), dto.getName(), dto.getAddress(), dto.getSalary()
         );
+
         customerRepo.save(c1);
-        return c1.getId()+" Saved!";
+        return c1.getId() + " Saved!";
     }
 
     @Override
