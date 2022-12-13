a41cd4160deca2ee1b1b69d00bd461db84bfc876
ozen-hub <info.ozenhub@gmail.com>
2022-12-13 20:38:17 +0530

    2

---
+++ src/main/java/com/bootcamp/pos/EzyPOS/entity/Customer.java
@@ -0,0 +1,17 @@
+package com.bootcamp.pos.EzyPOS.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer {
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
+}
---
+++ src/main/java/com/bootcamp/pos/EzyPOS/repo/CustomerRepo.java
@@ -0,0 +1,4 @@
+package com.bootcamp.pos.EzyPOS.repo;
+
+public interface CustomerRepo {
+}
