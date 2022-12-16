ac2d2c6199665c92e462596c18fbe99b19ca071b
ozen-hub <info.ozenhub@gmail.com>
2022-12-16 23:28:21 +0530

    25

--- src/main/java/com/bootcamp/pos/EzyPOS/repo/CustomerRepo.java
+++ src/main/java/com/bootcamp/pos/EzyPOS/repo/CustomerRepo.java
@@ -12,11 +12,11 @@ import java.util.List;
 @EnableJpaRepositories
 public interface CustomerRepo extends JpaRepository<Customer, String> {
 
-    @Query(value = "SELECT * FROM customer",nativeQuery = true)
+    @Query(value = "SELECT * FROM customer WHERE name LIKE %?1% || address LIKE %?1%",nativeQuery = true)
     public List<Customer> searchCustomer(String searchText, Pageable pageable);
-/* WHERE name LIKE%?0% OR address LIKE%?0%*/
-    @Query(value = "SELECT COUNT(id) FROM customer",
+/* */
+    @Query(value = "SELECT COUNT(id) FROM customer WHERE name LIKE %?1% || address LIKE %?1%",
             nativeQuery = true)
     public long countCustomer(String searchText);
-/* WHERE name LIKE %?0% OR address LIKE %?0%*/
+/* */
 }
